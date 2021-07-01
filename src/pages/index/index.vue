28d6cacd542728993c09e34cac32c797deb2eefa
yuyunhong <1162528696@qq.com>
2021-07-01 14:57:26 +0800

    feat: add tokenomics

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -6,8 +6,18 @@
       <div class="info" id="desc">
         <h1>{{ $t('message.index.info.title') }}</h1>
         <p v-html="$t('message.index.info.desc')"></p>
-        <div class="imgs"></div>
-        <img src="" />
+        <div class="card">
+          <div class="title">
+            {{ $t('message.index.info.tokenomics') }}
+          </div>
+          <div class="imgs">
+            <img class="img1" :src="cardImags.allocations[$i18n.locale]" />
+            <img class="img2" :src="cardImags.total[$i18n.locale]" />
+          </div>
+        </div>
+        <div class="card-mobile">
+          <img :src="cardImags.mobile[$i18n.locale]" />
+        </div>
       </div>
       <div class="activity-wrapper" id="activity">
         <h1>{{ $t('message.index.activity.title') }}</h1>
@@ -28,7 +38,13 @@
                 </div>
               </div>
               <div class="check-receive">
-                <el-button @click="handleShowAirdropModel" type="primary" size="medium" round :disabled="true">
+                <el-button
+                  @click="handleShowAirdropModel"
+                  type="primary"
+                  size="medium"
+                  round
+                  :disabled="true"
+                >
                   {{ $t('message.index.activity.airdropClaimEnded') }}
                 </el-button>
               </div>
@@ -78,6 +94,21 @@ import AirdropModel from './airdrop-model/airdrop-model.vue'
 import tabList from './tabs'
 import getProjects, { Project } from '@/service/getProjects'
 
+const cardImags = {
+  allocations: {
+    [LOCALES.EN]: require('./allocation-en.png'),
+    [LOCALES.ZH]: require('./allocation-zh.png')
+  },
+  total: {
+    [LOCALES.EN]: require('./total-en.png'),
+    [LOCALES.ZH]: require('./total-zh.png')
+  },
+  mobile: {
+    [LOCALES.EN]: require('./tokenomics-en.png'),
+    [LOCALES.ZH]: require('./tokenomics-zh.png')
+  }
+}
+
 export default {
   components: {
     AirdropModel
@@ -106,6 +137,7 @@ export default {
 
       airdropModel,
       handleShowAirdropModel,
+      cardImags,
 
       LOCALES,
       tableData: []
@@ -142,6 +174,79 @@ export default {
       white-space: normal;
       p {
         word-break: break-all;
+      }
+      .card {
+        position: relative;
+        padding: 20px 30px 20px 100px;
+        background: #b08ffe;
+        margin-top: 50px;
+        .title {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          position: absolute;
+          left: 0;
+          top: 50%;
+          transform: translateY(-50%);
+          width: 33px;
+          padding: 0 15px;
+          height: 184px;
+          background: #d62283;
+          border-radius: 0 20px 20px 0;
+          font-size: 20px;
+          font-weight: 600;
+          color: #ffffff;
+          margin: 0 auto;
+          line-height: 24px;
+          word-wrap: break-word;
+          text-align: center;
+          writing-mode: tb-rl;
+        }
+        .imgs {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          background: #fff;
+          border-radius: 29px;
+          padding: 30px 40px;
+          .img1 {
+            width: 462px;
+            height: 258px;
+          }
+          .img2 {
+            width: 300px;
+            height: 86px;
+          }
+        }
+      }
+      .card-mobile {
+        display: none;
+      }
+      @include tablet {
+        .card {
+          display: none;
+        }
+        .card-mobile {
+          display: block;
+          margin-top: 35px;
+          img {
+            width: 100%;
+            height: auto;
+          }
+        }
+      }
+      @include mobile {
+        .card {
+          display: none;
+        }
+        .card-mobile {
+          display: block;
+          margin-top: 35px;
+          img {
+            width: 100%;
+            height: auto;
+          }
+        }
       }
     }
     .activity-wrapper {
